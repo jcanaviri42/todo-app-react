7ddbb928326ef9b4714c07c608ce327411addf75
josue <jcanaviri20@gmail.com>
2022-02-07 18:09:15 -0400

    Add basic styles

--- src/App.jsx
+++ src/App.jsx
@@ -1,60 +1,83 @@
 import { useState } from 'react'
-import logo from './logo.svg'
+
 import './App.css'
 
-import Another from './Another'
-import Counter from './Counter'
-
-function App() {
-  const [count, setCount] = useState(0)
-
-  const someStyle = {
-    background: 'blue',
-    color: 'white',
-    fontSize: '28px',
-    fontWeight: 'bold'
-  }
+export default function App() {
+  const [todos, setTodos] = useState([
+    {
+      id: 1,
+      title: 'Finish React Series',
+      isComplete: false,
+    },
+    {
+      id: 2,
+      title: 'Go Grocery',
+      isComplete: true,
+    },
+    {
+      id: 3,
+      title: 'Take over world',
+      isComplete: false,
+    },
+  ])
 
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>Hello Vite + React!</p>
-        <Another />
-        <Counter initialCount={0}/>
-        <p>
-          <button type="button" onClick={() => { setCount(prevCount => prevCount + 1); setCount(prevCount => prevCount + 1) } }>
-            count is: {count}
-          </button>
+    <div className="todo-app-container">
+      <div className="todo-app">
+        <form>
+          <input
+            type="text"
+            className="todo-input"
+            placeholder="What do you need to do?"
+          />
+        </form>
 
-          <button type='button' onClick={() => setCount(count=> count - 1)}>Decrement</button>
-        </p>
-        <p style={someStyle}>{2 * 5}</p>
-        <p>
-          Edit <code>App.jsx</code> and save to test HMR updates.
-        </p>
-        <p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-          {' | '}
-          <a
-            className="App-link"
-            href="https://vitejs.dev/guide/features.html"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Vite Docs
-          </a>
-        </p>
-      </header>
+        <ul className="todo-list">
+          {todos.map((todo, index) => (
+            <li key={index} className="todo-item-container">
+              <div className="todo-item">
+                <input type="checkbox" />
+                <span className="todo-item-label">{todo.title}</span>
+              </div>
+
+              <button className="x-button">
+                <svg
+                  className="x-button-icon"
+                  fill="none"
+                  viewBox="0 0 24 24"
+                  stroke="currentColor"
+                >
+                  <path
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                    strokeWidth={2}
+                    d="M6 18L18 6M6 6l12 12"
+                  />
+                </svg>
+              </button>
+            </li>
+          ))}
+        </ul>
+
+        <div className="check-all-container">
+          <div>
+            <div className="button button-scale">Check All</div>
+          </div>
+          <span>3 items remaining</span>
+        </div>
+        <div className="other-buttons-container">
+          <div>
+            <button className="button filter-button filter-button-active">
+              All
+            </button>
+            <button className="button filter-button">Active</button>
+            <button className="button filter-button">Completed</button>
+          </div>
+          <div>
+            <button className="button button-scale">Clear completed</button>
+          </div>
+        </div>
+      </div>
     </div>
   )
 }
-
-export default App
