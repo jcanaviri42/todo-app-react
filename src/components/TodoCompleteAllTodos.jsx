f7dab097feabaa9999e2b64988fb88eeec7006ba
josue <jcanaviri20@gmail.com>
2022-02-09 10:34:09 -0400

    fix: bug while press check all button

--- src/components/TodoCompleteAllTodos.jsx
+++ src/components/TodoCompleteAllTodos.jsx
@@ -2,10 +2,13 @@ import { useContext } from 'react'
 import { TodosContext } from '../context/TodoContext'
 
 function TodoCompleteAllTodos() {
-  const {todos, setTodos} = useContext(TodosContext)
+  const { todos, setTodos } = useContext(TodosContext)
 
   const completeAllTodos = () => {
-    const updatedTodos = todos.map(todo => todo.isComplete = true)
+    const updatedTodos = todos.map(todo => { 
+      todo.isComplete = true
+      return todo
+    })
     setTodos(updatedTodos)
   }
 
