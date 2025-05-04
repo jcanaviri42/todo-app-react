8f763085cee91e8f4219ece8718abb40cee36bac
Josué Canaviri <31946002+jcanaviri42@users.noreply.github.com>
2025-05-03 23:38:43 -0400

    feat: delete some files

--- src/main.jsx
+++ src/main.jsx
@@ -3,8 +3,6 @@ import ReactDOM from 'react-dom/client'
 
 import './App.css'
 import App from './App'
-// import AppClass from './AppClass'
-// import Root from './Root'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <StrictMode>
